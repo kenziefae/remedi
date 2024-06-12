e09b5fade45e5be098eeac2efd8af20c8d7b2e2f
Kenzie FT <kenzieftptm@gmail.com>
2024-06-12 09:29:20 +0700

    remedi

---
+++ remedi.py
@@ -0,0 +1,28 @@
+# apkdesain = ["AdobePhotoshop", "AdobeIllustrator", "Inkscape", "AdobeInDesign", "CorelDRAWGraphic Suite", "AffinityDesigner", "Canva", "GIMP", "GravitDesigner", "Sketch"]
+
+# print ("apkdesain[0]: ", apkdesain[0:7])
+
+# English = {"book" : "buku", "pen" : "pena", "ruler" : "penggaris", "table" : "meja", "lamp" : "lampu", "window" : "jendela", "door" : "pintu", "floor" : "lantai", "chair" : "kursi", "fan" : "kipas", }
+
+# print ("english['book']: ", english['book'])
+# print ("english['pen']: ", english['pen'])
+# print ("english['ruler']: ", english['ruler'])
+# print ("english['table']: ", english['table'])
+# print ("english['lamp']: ", english['lamp'])
+# print ("english['window']: ", english['window'])
+# print ("english['door']: ", english['door'])
+# print ("english['floor']: ", english['floor'])
+# print ("english['chair']: ", english['chair'])
+# print ("english['fan']: ", english['fan'])
+
+
+# def trapesium(a = 5, b = 7, t = 10):
+#     sum = 0.5 * (a + b) * t
+#     print('sum:', sum)
+#     print('trapesium:', trapesium)
+
+
+
+
+
+  
